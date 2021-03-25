2bd919f74274175c8a14ee45a6d59bb7418d5f49
joshmed27 <joshuamed27@gmail.com>
2021-03-25 17:28:04 -0600

    Create Server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var http = require('http');
-var fs = require('fs');
+import { createServer } from 'http';
+import { readFile } from 'fs';
 
 const PORT=8080; 
 
-fs.readFile('./index.html', function (err, html) {
+readFile('./index.html', function (err, html) {
 
     if (err) throw err;    
 
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
+    createServer(function(request, response) {  
+        response.writeHead(200, {"Content-Type": "text/html"});  
         response.write(html);  
         response.end();  
     }).listen(PORT);
