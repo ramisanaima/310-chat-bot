930ffdb46b7ea85b4fca27cc785fb920f4a9232c
Shiyastra <74397306+Shiyastra@users.noreply.github.com>
2021-03-25 19:32:25 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ import express from 'express';
 import { readFile } from 'fs';
 
 const app = express();
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const PORT=8080; 
 
-readFile('./index.html', function (err, html) {
+app.use(express.static(__dirname + '/index'));
 
-    if (err) throw err;    
+app.use(express.static('public'));
+
+readFile('./public/index.html', function (err, html) {
+
+    if (err) throw err; 
 
     app.get('/', (request, response) => {
         response.writeHeader(200, {"Content-Type": "text/html"});  
