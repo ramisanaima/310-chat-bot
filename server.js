4f9603f9df50b7ae3971aba777015c25193c58ff
Shiyastra <74397306+Shiyastra@users.noreply.github.com>
2021-03-28 16:12:43 -0700

    Update server.js with wink-ner

--- server.js
+++ server.js
@@ -62,3 +62,44 @@ sentiment
   .getSentiment(string)
   .then(result => console.log(result));
 }
+
+
+// Load wink ner.
+// var ner = require( 'wink-ner' );
+import ner from 'wink-ner';
+// Create your instance of wink ner & use default config.
+var myNER = ner();
+// Define training data.
+var trainingData = [
+  { text: 'manchester united', entityType: 'club', uid: 'manu' },
+  { text: 'manchester', entityType: 'city' },
+  { text: 'U K', entityType: 'country', uid: 'uk' }
+];
+// Learn from the training data.
+myNER.learn( trainingData );
+// Since recognize() requires tokens, use wink-tokenizer.
+//var winkTokenizer = require( 'wink-tokenizer' );
+import winkTokenizer from 'wink-tokenizer';
+// Instantiate it and extract tokenize() api.
+var tokenize = winkTokenizer().tokenize;
+// Tokenize the sentence.
+var tokens = tokenize( 'Manchester United is a football club based in Manchester, U. K.' );
+// Simply Detect entities!
+tokens = myNER.recognize( tokens );
+console.log( tokens );
+// -> [
+//      { entityType: 'club', uid: 'manu', originalSeq: [ 'Manchester', 'United' ],
+//        value: 'manchester united', tag: 'word' },
+//      { value: 'is', tag: 'word' },
+//      { value: 'a', tag: 'word' },
+//      { value: 'football', tag: 'word' },
+//      { value: 'club', tag: 'word' },
+//      { value: 'based', tag: 'word' },
+//      { value: 'in', tag: 'word' },
+//      { entityType: 'city', value: 'Manchester', tag: 'word',
+//        originalSeq: [ 'Manchester' ], uid: 'manchester' },
+//      { value: ',', tag: 'punctuation' },
+//      { entityType: 'country', uid: 'uk', originalSeq: [ 'U', '.', 'K' ],
+//        value: 'u k', tag: 'word' },
+//      { value: '.', tag: 'punctuation' }
+//    ]
