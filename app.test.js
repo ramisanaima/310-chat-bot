58089f562828fe5cd1f5219a43508ee43a7978a2
ramisanaima <ramisanaima@gmail.com>
2021-03-30 02:35:30 +0600

    re-install jest
    re-install jest and debug

--- app.test.js
+++ app.test.js
@@ -1,8 +1,18 @@
 
-const clean = require('./app');
 
-test('output clean string', () => {
-    expect(clean('Hello whats up?! ^.^')).toBe('hello whats up ')
-})
+const {clean} = require('./public/app');
+const {compare} = require('./public/app');
+
+test( 'should output clean userInput', ()=> {
+    const text = clean('Hello whats up?! ^.^');
+    expect(text).toBe('hello whats up ');
+});
+
+
+test( 'should compare input with reply', ()=> {
+    const text = compare(prompts, replies, 'im good');
+    expect(text).toBe('I am glad to hear that! Could I please get your name?');
+});
+
 
 
